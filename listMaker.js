9d904ca6fd851ce3e3fed5e2c4bdef5af548b691
Nehemiah Mekonnen <nmmekonnen@gmail.com>
2018-12-12 21:14:21 -0500

    added remove from list function

--- listMaker.js
+++ listMaker.js
@@ -1,12 +1,14 @@
-
-
 //User object
-function User(name, addToList, list) {
+function User(name, addToList, list, removeFromList) {
   this.name = name;
   this.list = list = [];
 
   //Adds a list of to-do tasks to the array
   this.addToList = function addToList(task) {
     list.push(task);
+  }
+
+  this.removeFromList = function removeFromList(task) {
+    list.pop(task);
   }
 }
